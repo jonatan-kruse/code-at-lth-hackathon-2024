f296caf18ed9b92bba25ce39692e1abcf16d5fab
Jonatan Kruse <jonatankruse@hotmail.com>
2024-05-19 14:11:08 +0200

    p1 done

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -1,8 +1,17 @@
-use std::io::{self, BufRead};
+use std::{collections::HashSet, io::{self, BufRead}};
 
 fn main() {
     let mut stdin = io::stdin().lock().lines();
     let num: usize = stdin.next().unwrap().unwrap().parse().unwrap();
-    let name = stdin.next().unwrap().unwrap();
-    println!("{}", num * name.len());
+    let mut known_words = HashSet::new();
+    for _ in 0..num {
+        let new_word = stdin.next().unwrap().unwrap().to_lowercase();
+        known_words.insert(new_word);
+    }
+    let test_words = stdin.next().unwrap().unwrap().to_lowercase();
+    if test_words.split_ascii_whitespace().all(|word| known_words.contains(word)) {
+        println!("Hi, how do I look today?");
+    } else {
+        println!("Thore has left the chat");
+    }
 }
