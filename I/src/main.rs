6912c91c97734ef99d4d086f5da38a18fee54839
Jonatan Kruse <jonatankruse@hotmail.com>
2024-05-19 20:29:23 +0200

    Remove uncompleted problems

--- I/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::io::{self, BufRead};
-
-fn main() {
-    let mut stdin = io::stdin().lock().lines();
-    let num = stdin.next().unwrap().unwrap().parse::<usize>().unwrap();
-    let name = stdin.next().unwrap().unwrap();
-    println!("{}", num * name.len());
-}
