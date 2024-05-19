2bdea9220b546230bb4df4dbc7d197323e567609
Jonatan Kruse <jonatankruse@hotmail.com>
2024-05-19 15:22:39 +0200

    Improve D

--- D/src/main.rs
+++ D/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, io::{self, BufRead}};
+use std::io::{self, BufRead};
 
 fn main() {
     let mut stdin = io::stdin().lock().lines();
@@ -13,20 +13,24 @@ fn main() {
         let credits: usize = credits.parse().unwrap();
         corses.push((credits, lecturer.to_owned()));
     }
-    let max_score = pick_corse(Vec::new(), corses, max_credit);
+    let max_score = pick_corse(0, corses, max_credit);
     println!("{max_score}");
 }
 
-fn pick_corse(picked: Vec<(usize, String)>, all: Vec<(usize, String)>, max_score: usize) -> usize {
-    let lecturers = picked.iter().map(|i| i.1.to_owned()).collect::<HashSet<_>>();
-    let score = picked.iter().map(|i| i.0).sum();
-    all.iter().map(|(credits, lecturer)| {
-        if credits + score <= max_score && !lecturers.contains(lecturer) {
-            let mut new_picked = picked.clone();
-            new_picked.push((*credits, lecturer.to_owned()));
-            pick_corse(new_picked, all.to_owned(), max_score)
-        } else {
-            score
-        }
-    }).max().unwrap()
+fn pick_corse(score: usize, all: Vec<(usize, String)>, max_score: usize) -> usize {
+    all.iter()
+        .map(|(credits, lecturer)| {
+            if credits + score <= max_score {
+                let new_all = all
+                    .clone()
+                    .into_iter()
+                    .filter(|(_, lect)| lect != lecturer)
+                    .collect();
+                pick_corse(credits + score, new_all, max_score)
+            } else {
+                score
+            }
+        })
+        .max()
+        .unwrap_or(score)
 }
